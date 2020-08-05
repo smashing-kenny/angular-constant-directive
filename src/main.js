ca33cfa5b2ee69484606d630aed2778337b1bc3b
Eduards Gruberts <eduards.gruberts@gmail.com>
2020-08-05 23:21:54 +0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,23 @@
+angular.module('ConstantDirective',[])
+	.directive('ngConstant', ['$ConstantInjector', function($ConstantInjector){	
+		return {
+			restrict: 'A',
+			transclude: true,
+			link: function(scope, element, attrs) {
+				$ConstantInjector.constant(
+					attrs.ngConstant,
+					JSON.parse(element.html()));
+
+					element.remove();
+			}
+		};
+	}])
+	.provider('$ConstantInjector', ['$provide',function($provide){
+		this.$get = [function(){
+			return {
+				constant: function(name, value){
+					$provide.constant(name, value);
+				}
+			}
+		}];
+	}]);
